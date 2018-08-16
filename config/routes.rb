9089038c6c29640da3c1e220b6849512651218e6
Taylour <29444471+tailorem@users.noreply.github.com>
2018-08-16 19:37:31 +0000

    adds user registration links and forms

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,19 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
+
+  # resources :users, only: [:new, :create]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show, :destory]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
